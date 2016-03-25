e5e5fe2934ea3301495c38bee57c45fe1847159e
Confused Anime Girl Who Isn't Very Good With Code <anonymous@cock.lu>
2016-03-25 09:15:34 -0700

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+#include <string.h>
+#include <ctime>
+#include <map>
+#include <vector>
+
+//Global Class of information related to the user and all of the information related to them
+class user{
+public:
+  std::string FName;
+  std::string LName;
+  bool isFemale?;
+  int age;
+  std::map facts; //an association off named attributes with strings that can be recalled for further discussion
+}
+
+//function prototyping
+std::string Round();
+
+
+int main()
+{
+  
+}
+
+//Round encompases a normal set of exchanges and will be teh function that interacts will all the others as well as manages them.
+std::string Round(std::string line)
+{
+
+}
